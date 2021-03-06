a085c48921666f2a939b09459e507252b90f2ca4
achintya ranjan chaudhary <achintya.ranjan1@hashedin.com>
2021-03-07 00:31:28 +0530

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className='navbar'>
+            <h1>dojo bar</h1>
+            <div className="links">
+            <a href="/">Home</a>
+            <a href="/content">Content</a>
+            </div>
+            
+        </nav>
+    );
+}
+
+export default Navbar;
